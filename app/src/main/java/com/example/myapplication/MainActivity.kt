37c1cf657de3e66912ac309ffcd13f3fc9824033
tsl4398 <senglim.tsl@gmail.com>
2019-10-16 23:47:10 +0800

    app first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,25 +4,51 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-val rollButton: Button = findViewById((R.id.roll_button))
-        rollButton.setOnClickListener { rollDice()}
-}
+        val rollButton: Button = findViewById((R.id.roll_button))
+        rollButton.setOnClickListener { rollDice() }
+
+        val countButton: Button = findViewById(R.id.countup_button)
+        countButton.setOnClickListener { countUp() }
+    }
 
     private fun rollDice()
     {
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
 
-        val random = Random().nextInt(bound 6) + 1
+        val randomInt = Random().nextInt(6) + 1
 
-        val result : TextView = findViewById(R.id.result_text)
+        val result_text : TextView = findViewById(R.id.result_text)
         result_text.text = randomInt.toString()
+    }
+
+    private fun countUp()
+    {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if (resultText.text == "Hello World!")
+        {
+            resultText.text = "1"
+        }
+        else
+        {
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6)
+            {
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
     }
 }
